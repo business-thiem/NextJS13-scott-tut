f6a31f2ccbeda03b94d3e508d7dcefcdb8f58e52
Thiem <thiem.huynh0@gmail.com>
2023-08-18 15:22:39 -0400

    prisma, layout, page init

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Todos Dashboard</h1>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
