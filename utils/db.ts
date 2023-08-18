cede0a03f1b374e2222342a09d7e1e3fc6c74aec
Thiem <thiem.huynh0@gmail.com>
2023-08-18 15:35:32 -0400

    util init

---
+++ utils/db.ts
@@ -0,0 +1,16 @@
+// reference from: https://github.com/Hendrixer/intro-nextjs-v3/blob/main/utils/db.ts
+
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
